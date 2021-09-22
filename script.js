946bd2d1dc1aaf1210959f44fa08c5bf231cd8a6
maxmotsyk <21mechanic21@gmail.com>
2021-09-22 22:16:04 +0300

    add slider

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// burger menu
 const byrgerDoActive = function (iconBurger){
     if(iconBurger){
         const menu_left  =  document.querySelector('.menu_left');
@@ -15,3 +15,24 @@ const byrgerDoActive = function (iconBurger){
 const iconMenu = document.querySelector('.burger_menu');
 byrgerDoActive(iconMenu);
 
+// slider
+
+let sliders = document.querySelectorAll('.screen');
+let slider_mass = [];
+console.log(sliders);
+for(let i = 0; i < sliders.length; i++ ){
+    slider_mass[i] = sliders[i].src;
+    sliders[i].remove();
+}
+
+console.log(slider_mass);
+
+let step = 0;
+let offset = 0;
+
+function drow(){
+   let img =  document.createElement('img');
+   img.src = slider_mass[step];
+   img.classList.add('screen');
+   img.style
+}
